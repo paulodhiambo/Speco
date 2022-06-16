eced6ab6acaec0143d68e608590b429bbc87e636
Paul Odhiambo <paulodhiambo962@gmail.com>
2022-06-16 15:57:48 +0300

    speso UI challenge

---
+++ app/src/main/java/com/speco/speco/ui/services/ServicesViewModel.kt
@@ -0,0 +1,13 @@
+package com.speco.speco.ui.services
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ServicesViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
